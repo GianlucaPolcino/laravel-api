3811a14d5d15e07be61150ab837f47b10679dc58
Gianluca.Polcino <gianluca.polcino.gp@gmail.com>
2022-02-10 15:45:21 +0100

    vue created

---
+++ resources/js/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <Header />
+      <h1>
+          Ciao
+      </h1>
+  </div>
+</template>
+
+<script>
+
+import Header from "./components/partials/Header.vue";
+
+export default {
+    name: 'App',
+
+    components:{
+        Header,
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ resources/js/components/partials/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+      <nav>
+          <ul>
+              <li>
+                  <a href="#">Home</a>
+                  <a href="#">Blog</a>
+                  <a href="#">Chi siamo</a>
+                  <a href="#">Contatti</a>
+              </li>
+          </ul>
+      </nav>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
